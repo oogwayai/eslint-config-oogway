b59a2b99f0787f49164253ffc2d80bfcb7a599f5
Ankur Agarwal <agarwalankur58@gmail.com>
2021-04-15 20:06:04 +0530

    add config base

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  plugins: [
+    'react-hooks',
+  ],
+  rules: {
+  },
+};
